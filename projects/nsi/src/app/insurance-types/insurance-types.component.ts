55acdbb4b304c9d7340ac5e237ace611e89cd064
Andrey Zinchenko <don7andrew@gmail.com>
2021-03-03 04:03:13 +0300

    improved layout, added filters

--- projects/nsi/src/app/insurance-types/insurance-types.component.ts
+++ projects/nsi/src/app/insurance-types/insurance-types.component.ts
@@ -14,11 +14,20 @@ export class InsuranceTypesComponent implements OnInit {
 
   private tableData: IHandbookRow[] = [];
   private filteredData: IHandbookRow[] = [];
+  
   displayData: IHandbookRow[] = [];
   currentUser?: string;
   currentRole?: string;
   info: string = '';
   tableNav  = { current: 1, total: 1, max: 1, rows: 10 }
+  filters = {
+    recordStatus: '',
+    codeStatus: '',
+    changedFrom: '',
+    changedTo: '',
+    activeFrom: '',
+    activeTo: '',
+  };
 
   constructor(private dataService: DataService, private userSession: UserSessionService) { }
 
@@ -34,20 +43,72 @@ export class InsuranceTypesComponent implements OnInit {
     console.log('init insurance');
 
   }
+  onApplyFilters():void {
+    console.log(this.filters);
+    this.filteredData = this.tableData;
+
+    // record
+    if (this.filters.recordStatus != '') {
+      this.filteredData = this.filteredData
+      .filter(e => { return (e.recordStatus === this.filters.recordStatus) ? true : false; });
+    }
+
+    // code
+    if (this.filters.codeStatus != '') {
+      this.filteredData = this.filteredData
+      .filter(e => { 
+        if ('Введен' === this.filters.codeStatus) {
+          return (1 == this.compareDateStringToNow(e.codeEndDate)) ? true : false; 
+        }
+        if ('Закрыт' === this.filters.codeStatus) {
+          return (-1 == this.compareDateStringToNow(e.codeEndDate)) ? true : false; 
+        }
+        return false;
+      });
+    }
+
+    // record period
+    if (this.filters.activeFrom != '') {
+      this.filteredData = this.filteredData.filter(e => {
+        return (new Date(this.filters.activeFrom).getTime() < 
+          this.dateFromCustomString(e.recordStartDate).getTime()) ? true : false;
+      });
+    }
+    if (this.filters.activeTo != '') {
+      this.filteredData = this.filteredData.filter(e => {
+        return (new Date(this.filters.activeTo).getTime() > 
+          this.dateFromCustomString(e.recordEndDate).getTime()) ? true : false;
+      });
+    }
+
+    this.tableNav.max = Math.ceil(this.filteredData.length / this.tableNav.rows);
+    this.setDisplayData(1);
+    this.info =`Найдено записей: ${this.filteredData.length}`;
+  }
+  onClearFilters(): void {
+    for (let e in this.filters) {
+      (<any>this.filters)[e as keyof any] = '';
+    }
+    this.filteredData = this.tableData;
+    this.tableNav.max = Math.ceil(this.filteredData.length / this.tableNav.rows);
+    this.setDisplayData(1);
+    this.info = `Всего записей: ${this.filteredData.length}`;
+  }
   onSearchEnter(e: any): void {
     let text: string = e.target.value;
     if (text === '') {
       this.filteredData = this.tableData;
+      this.info = `Всего записей: ${this.filteredData.length}`;
     } else {
       this.filteredData = this.tableData.filter(e => {
         if (e.fullname.search(new RegExp(text, 'i')) === -1) return false;
         else return true;
       });
+      this.info =`Найдено записей: ${this.filteredData.length}`;
     }
     this.tableNav.max = Math.ceil(this.filteredData.length / this.tableNav.rows);
-    this.info =`Найдено записей: ${this.filteredData.length}`;
     this.setDisplayData(1);
-    console.log(`Enter: ${e.target.value}`);
+    // console.log(`Enter: ${e.target.value}`);
   }
   onTableNavigation(type: string, page: string='0'): void {
     switch (type) {
@@ -69,7 +130,7 @@ export class InsuranceTypesComponent implements OnInit {
         console.log(`Page input: ${page}`);
         break;   
       }
-    console.log(`Page: ${this.tableNav.current}`);
+    // console.log(`Page: ${this.tableNav.current}`);
   }
 
   private setTablePage(val: number): void {
@@ -77,7 +138,7 @@ export class InsuranceTypesComponent implements OnInit {
     if (val < 1 || val > this.tableNav.max) return;
     else if (this.tableNav.current !== val) {      
       this.setDisplayData(val);
-      console.log(`Set input to: ${this.tableNav.current}`);
+      // console.log(`Set input to: ${this.tableNav.current}`);
     }
   }
   private setDisplayData(page: number): void {
@@ -85,8 +146,24 @@ export class InsuranceTypesComponent implements OnInit {
     let startIdx =  this.tableNav.rows * (page - 1);
     this.displayData = this.filteredData.slice(startIdx, startIdx + this.tableNav.rows);
   }
+  // 1 future, -1 past
+  private compareDateStringToNow(tdate: string): number {
+    let d: number = new Date(parseInt(tdate.substring(6,10)), parseInt(tdate.substring(3,7))-1,
+    parseInt(tdate.substring(0,2))).getTime();
+
+    if (d === Date.now()) return 0;
+    else if (d > Date.now()) return 1;
+    else return -1;
+
+  }
+  private dateFromCustomString(date: string): Date {
+    return new Date(parseInt(date.substring(6,10)), parseInt(date.substring(3,7))-1,
+    parseInt(date.substring(0,2)));
+  }
   public debug(): void {
-    console.log(Date.now());
+    console.log(this.filters);
+    // console.log((new Date()).toLocaleDateString());
+    console.log(this.compareDateStringToNow('12.12.2010'));
   }
 
 }
--- projects/nsi/src/app/tsfiles/mock-table-data.ts
+++ projects/nsi/src/app/tsfiles/mock-table-data.ts
@@ -13,22 +13,22 @@ export class HandbookData {
     private static status: string[] = ['Опубликованная', 'Новая'];
     private static code: string[] = ['IC_PROP', 'IC_CTR_OP'];
     private static getMockDate(): string {
-        return new Date(Date.now()-Math.random()*20*365*24*60*60*1000).toLocaleDateString().replace('-','.');
+        return new Date(Date.now()-Math.random()*20*365*24*60*60*1000+10*365*24*60*60*1000).toLocaleDateString().replace('-','.');
     }
 
     public static getData(rows: number): IHandbookRow[] {
-        // let _data: IHandbookRow[] = [];
         this.data = [];
         for (let i = 0; i < rows; i++) {
+            let date: number = (Date.now()-Math.random()*20*365*24*60*60*1000+10*365*24*60*60*1000); 
             this.data.push({
                 fullname: `Запись справочника №${i+1}`,
                 recordStatus: this.status[Math.floor(Math.random()*2)],
                 code: this.code[Math.floor(Math.random()*2)],
-                recordStartDate: this.getMockDate(),
-                recordEndDate: this.getMockDate(),
+                recordStartDate: new Date(date).toLocaleDateString(),
+                recordEndDate: new Date(date+Math.random()*5*365*24*60*60*1000).toLocaleDateString(),
                 codeEndDate: this.getMockDate(),
             });
-            
+
         }
         // this.data = _data;   
         return this.data;
