de65dce2d8038c3583faec0ce28736546a629e19
Andrey Zinchenko <don7andrew@gmail.com>
2021-03-15 09:47:25 +0300

    micro refactoring

--- projects/nsi/src/app/insurance-types/insurance-types.component.ts
+++ projects/nsi/src/app/insurance-types/insurance-types.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../SERVICES/data.service';
-import { UserSessionService } from "../SERVICES/user-session.service";
-import { IHandbookRow } from "../tsfiles/mock-table-data";
+import { UserSessionService } from '../SERVICES/user-session.service';
+import { IHandbookRow } from '../tsfiles/mock-table-data';
 
 @Component({
   selector: 'app-insurance-types',
@@ -14,10 +14,10 @@ export class InsuranceTypesComponent implements OnInit {
 
   private tableData: IHandbookRow[] = [];
   private filteredData: IHandbookRow[] = [];
-  
+
   displayData: IHandbookRow[] = [];
-  session = { currentUser: '', currentRole: '',};
-  info: string = '';
+  session = { currentUser: '', currentRole: '', };
+  info = '';
   tableNav  = { current: 1, total: 1, max: 1, rows: 10 };
   filters = {
     recordStatus: 'Все',
@@ -42,25 +42,25 @@ export class InsuranceTypesComponent implements OnInit {
     console.log('init insurance');
 
   }
-  onApplyFilters():void {
+  onApplyFilters(): void {
     console.log(this.filters);
     this.filteredData = this.tableData;
 
     // record
     if (this.filters.recordStatus != 'Все') {
       this.filteredData = this.filteredData
-      .filter(e => { return (e.recordStatus === this.filters.recordStatus) ? true : false; });
+      .filter(e => (e.recordStatus === this.filters.recordStatus));
     }
 
     // code
     if (this.filters.codeStatus != 'Все') {
       this.filteredData = this.filteredData
-      .filter(e => { 
+      .filter(e => {
         if ('Введен' === this.filters.codeStatus) {
-          return (1 == this.compareDateStringToNow(e.codeEndDate)) ? true : false; 
+          return (1 === this.compareDateStringToNow(e.codeEndDate));
         }
         if ('Закрыт' === this.filters.codeStatus) {
-          return (-1 == this.compareDateStringToNow(e.codeEndDate)) ? true : false; 
+          return (-1 == this.compareDateStringToNow(e.codeEndDate));
         }
         return false;
       });
@@ -69,20 +69,20 @@ export class InsuranceTypesComponent implements OnInit {
     // record period
     if (this.filters.activeFrom != '') {
       this.filteredData = this.filteredData.filter(e => {
-        return (new Date(this.filters.activeFrom+'T00:00:00').getTime() <= 
-          this.dateFromCustomString(e.recordStartDate).getTime()) ? true : false;
+        return (new Date(this.filters.activeFrom + 'T00:00:00').getTime() <=
+          this.dateFromCustomString(e.recordStartDate).getTime());
       });
     }
     if (this.filters.activeTo != '') {
       this.filteredData = this.filteredData.filter(e => {
-        return (new Date(this.filters.activeTo+'T00:00:00').getTime() >= 
-          this.dateFromCustomString(e.recordEndDate).getTime()) ? true : false;
+        return (new Date(this.filters.activeTo + 'T00:00:00').getTime() >=
+          this.dateFromCustomString(e.recordEndDate).getTime());
       });
     }
 
     this.tableNav.max = Math.ceil(this.filteredData.length / this.tableNav.rows);
     this.setDisplayData(1);
-    this.info =`Найдено записей: ${this.filteredData.length}`;
+    this.info = `Найдено записей: ${this.filteredData.length}`;
   }
   onClearFilters(): void {
     // for (let e in this.filters) {
@@ -102,22 +102,21 @@ export class InsuranceTypesComponent implements OnInit {
     this.info = `Всего записей: ${this.filteredData.length}`;
   }
   onSearchEnter(e: any): void {
-    let text: string = e.target.value;
+    const text: string = e.target.value;
     if (text === '') {
       this.filteredData = this.tableData;
       this.info = `Всего записей: ${this.filteredData.length}`;
     } else {
       this.filteredData = this.tableData.filter(e => {
-        if (e.fullname.search(new RegExp(text, 'i')) === -1) return false;
-        else return true;
+        return e.fullname.search(new RegExp(text, 'i')) !== -1;
       });
-      this.info =`Найдено записей: ${this.filteredData.length}`;
+      this.info = `Найдено записей: ${this.filteredData.length}`;
     }
     this.tableNav.max = Math.ceil(this.filteredData.length / this.tableNav.rows);
     this.setDisplayData(1);
     // console.log(`Enter: ${e.target.value}`);
   }
-  onTableNavigation(type: string, page: string='0'): void {
+  onTableNavigation(type: string, page: string= '0'): void {
     switch (type) {
       case 'top-left':
         this.setTablePage(1);
@@ -132,40 +131,42 @@ export class InsuranceTypesComponent implements OnInit {
         this.setTablePage(this.tableNav.current + 1);
         break;
       case 'input':
-        let p = parseInt(page);
-        if (!isNaN(p)) this.setTablePage(p);
+        const p = parseInt(page, 10);
+        if (!isNaN(p)) { this.setTablePage(p); }
         console.log(`Page input: ${page}`);
-        break;   
+        break;
       }
     // console.log(`Page: ${this.tableNav.current}`);
   }
 
   private setTablePage(val: number): void {
     // console.log('set table page');
-    if (val < 1 || val > this.tableNav.max) return;
-    else if (this.tableNav.current !== val) {      
+    if (val < 1 || val > this.tableNav.max) { return; }
+    else if (this.tableNav.current !== val) {
       this.setDisplayData(val);
       // console.log(`Set input to: ${this.tableNav.current}`);
     }
   }
   private setDisplayData(page: number): void {
     this.tableNav.current = page;
-    let startIdx =  this.tableNav.rows * (page - 1);
+    const startIdx =  this.tableNav.rows * (page - 1);
     this.displayData = this.filteredData.slice(startIdx, startIdx + this.tableNav.rows);
   }
   // 1 future, -1 past
+  // noinspection JSMethodCanBeStatic
   private compareDateStringToNow(tdate: string): number {
-    let d: number = new Date(parseInt(tdate.substring(6,10)), parseInt(tdate.substring(3,7))-1,
-    parseInt(tdate.substring(0,2))).getTime();
+    const d: number = new Date(parseInt(tdate.substring(6, 10), 10), parseInt(tdate.substring(3, 7), 10) - 1,
+    parseInt(tdate.substring(0, 2), 10)).getTime();
 
-    if (d === Date.now()) return 0;
-    else if (d > Date.now()) return 1;
-    else return -1;
+    if (d === Date.now()) { return 0; }
+    else if (d > Date.now()) { return 1; }
+    else { return -1; }
 
   }
+  // noinspection JSMethodCanBeStatic
   private dateFromCustomString(date: string): Date {
-    return new Date(parseInt(date.substring(6,10)), parseInt(date.substring(3,7))-1,
-    parseInt(date.substring(0,2)));
+    return new Date(parseInt(date.substring(6, 10), 10), parseInt(date.substring(3, 7), 10) - 1,
+    parseInt(date.substring(0, 2), 10));
   }
   public debug(): void {
     console.log(this.filters);
